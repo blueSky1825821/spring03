09dd361a93410d328fecf3f6a973e20a21382333
wangmin <wangmin@yuantutech.com>
2016-12-23 10:42:20 +0800

    访问静态页面

--- src/main/java/com/spring/tutorialspoint/www/WebController.java
+++ src/main/java/com/spring/tutorialspoint/www/WebController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by sky on 2016/12/23.
+ * 	Spring Page Redirection Example
  */
 @Controller
 public class WebController {
@@ -25,5 +26,16 @@ public class WebController {
     public String finalPage() {
 
         return "final";
+    }
+
+    @RequestMapping(value = "/index3.do1", method = RequestMethod.GET)
+    public String index3() {
+        return "index3";
+    }
+
+    @RequestMapping(value = "/staticPage.do1", method = RequestMethod.GET)
+    public String redirect3() {
+//        访问WEB-INF目录下的静态页面怎么访问
+        return "redirect:/html/final.html";
     }
 }
