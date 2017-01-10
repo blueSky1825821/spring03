8b350bf303aa5fbf008831baefa03fcad0e77352
wangmin <wangmin@yuantutech.com>
2017-01-10 20:58:34 +0800

    测试

--- src/main/java/com/spring/beans/TestComponent.java
+++ src/main/java/com/spring/beans/TestComponent.java
@@ -1,12 +1,19 @@
 package com.spring.beans;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by sky on 2016/12/28.
  */
 
+//测试时自动创建Spring应用上下文
+@RunWith(SpringJUnit4ClassRunner.class)
+//加载bean
+@ContextConfiguration(classes = Myconfig.class)
 public class TestComponent {
 
     @Autowired
@@ -16,4 +23,5 @@ public class TestComponent {
     public void test01() {
         my.shout();
     }
+
 }
---
+++ src/main/java/com/test/test/Test01.java
@@ -0,0 +1,11 @@
+package com.test.test;
+
+/**
+ * Created by sky on 2017/1/5.
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        String s = String.valueOf(Double.parseDouble("10") / 100);
+        System.out.println(s);
+    }
+}
